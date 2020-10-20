8f1ab0bb80b64a9df6a894d310dfea18303ba4e9
Francisco Mello <42641623+FranciscoMelloJr@users.noreply.github.com>
2020-10-20 01:38:34 -0300

    fechou

--- Principal Léxico/Projeto/LA_Lexico.java
+++ Principal Léxico/Projeto/LA_Lexico.java
@@ -3,11 +3,15 @@ package Projeto;
 public class LA_Lexico {
 
 	public static void main(String[] args) {
-		
+
 		LA_Lex lex = new LA_Lex(args[0]);
+		String txt = "Lexemas encontrados: ";
 		Token t = null;
+
 		while ((t = lex.proximoToken()).nome != TipoToken.Fim) {
 			System.out.print(t);
+			txt += t.lexema + ", ";
 		}
+		System.out.print(txt);
 	}
 }
